a81fc8976de3ca744d6a7d56b187b1ff0f9d76fd
„Tuan <tuan38@seznam.czgit config --global user.name Tuan>
2023-08-10 10:45:54 +0200

    JUnit

---
+++ src/test/java/com/engeto/ProjektRegistracnisystem/UserControllerTest.java
@@ -0,0 +1,28 @@
+package com.engeto.ProjektRegistracnisystem;
+
+import com.engeto.ProjektRegistracnisystem.controllers.UserController;
+import com.engeto.ProjektRegistracnisystem.model.User;
+import com.engeto.ProjektRegistracnisystem.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTest {
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
+}
