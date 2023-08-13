4ea744ca1492f60750f524b6f65ee7c6b83718f9
Tuan Vu Minh <tuan38@seznam.cz>
2023-08-13 21:14:21 +0200

    Finální verze

--- src/main/java/com/engeto/ProjektRegistracnisystem/settings/WebConfig.java
+++ src/main/java/com/engeto/ProjektRegistracnisystem/settings/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/**") // Nahraď tohle za cestu tvého API
-                .allowedOrigins("*") // Povol všechny zdroje (pro vývoj můžeš nastavit konkrétní doménu)
-                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Povolené HTTP metody
+        registry.addMapping("/api/v1/**") 
+                .allowedOrigins("*") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); 
     }
 }
